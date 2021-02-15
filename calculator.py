70c1c16910a1f9d1cb072af30249bee7a9c8ff83
jainsiddharth99 <groupofsjain@gmail.com>
2021-02-15 14:32:12 +0530

    some changes

--- calculator.py
+++ calculator.py
@@ -5,24 +5,54 @@ e=Entry(root,width='35',borderwidth=5)
 e.grid(row=0,column=0,columnspan=4,padx=10,pady=10)
 # e.insert(0,"")
 
-def buttonadd(number):
-    return 
+def buttonclick(number):
+    current=e.get()
+    e.delete(0, END)
+    e.insert(0,str(current)+str(number))
+    
+def buttonclear():
+    e.delete(0,END)
+    
+def add():
+    first_num=e.get()
+    global f_num
+    global math
+    math="addition"
+    f_num=int(first_num)
+    e.delete(0,END)
+    
+def sub():   
+    first_num=e.get()
+    global f_num
+    global math
+    math="subtract"
+    f_num=int(first_num)
+    e.delete(0,END)
 
+def equal():
+    second_num=e.get()
+    e.delete(0,END)
+    if math=="addition":
+        e.insert(0,f_num+int(second_num))
+    elif math=="subtract":
+        e.insert(0,f_num-int(second_num))
+        
+        
 # buttons
-button1=Button(root,text="1",padx=40,pady=20,command=Lambda: buttonclick())
-button2=Button(root,text="2",padx=40,pady=20,command=Lambda: buttonclick())
-button3=Button(root,text="3",padx=40,pady=20,command=Lambda: buttonclick())
-button4=Button(root,text="4",padx=40,pady=20,command=Lambda: buttonclick())
-button5=Button(root,text="5",padx=40,pady=20,command=Lambda: buttonclick())
-button6=Button(root,text="6",padx=40,pady=20,command=Lambda: buttonclick())
-button7=Button(root,text="7",padx=40,pady=20,command=Lambda: buttonclick())
-button8=Button(root,text="8",padx=40,pady=20,command=Lambda: buttonclick())
-button9=Button(root,text="9",padx=40,pady=20,command=Lambda: buttonclick())
-button0=Button(root,text="0",padx=40,pady=20,command=Lambda: buttonclick())
-buttonadd=Button(root,text="+",padx=39,pady=20,command=Lambda: buttonclick())
-buttonsub=Button(root,text="-",padx=40,pady=20,command=Lambda: buttonclick())
-buttonclear=Button(root,text="Clear", padx=30,pady=53,command= Lambda: buttonclick())
-buttonequal=Button(root,text="=",padx=40,pady=51,command=Lambda: buttonclick())
+button1=Button(root,text="1",padx=40,pady=20,command=lambda: buttonclick(1))
+button2=Button(root,text="2",padx=40,pady=20,command=lambda: buttonclick(2))
+button3=Button(root,text="3",padx=40,pady=20,command=lambda: buttonclick(3))
+button4=Button(root,text="4",padx=40,pady=20,command=lambda: buttonclick(4))
+button5=Button(root,text="5",padx=40,pady=20,command=lambda: buttonclick(5))
+button6=Button(root,text="6",padx=40,pady=20,command=lambda: buttonclick(6))
+button7=Button(root,text="7",padx=40,pady=20,command=lambda: buttonclick(7))
+button8=Button(root,text="8",padx=40,pady=20,command=lambda: buttonclick(8))
+button9=Button(root,text="9",padx=40,pady=20,command=lambda: buttonclick(9))
+button0=Button(root,text="0",padx=40,pady=20,command=lambda: buttonclick(0))
+buttonadd=Button(root,text="+",padx=39,pady=20,command=add)
+buttonsub=Button(root,text="-",padx=40,pady=20,command=sub)
+buttonclear=Button(root,text="Clear", padx=30,pady=53,command= buttonclear)
+buttonequal=Button(root,text="=",padx=40,pady=51,command=equal)
 
 
 # on screen,
